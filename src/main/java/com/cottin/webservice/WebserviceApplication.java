179e61319367f7aba2424c963e53303ac6bb50cd
Tom Cottin <ctom.99@outlook.fr>
2022-06-01 12:24:49 +0200

    Enable Cross-Origin Resource Sharing (CORS)

--- src/main/java/com/cottin/webservice/WebserviceApplication.java
+++ src/main/java/com/cottin/webservice/WebserviceApplication.java
@@ -1,13 +1,35 @@
 package com.cottin.webservice;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class WebserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebserviceApplication.class, args);
+	}
+
+	@Bean
+	public CorsFilter CorsFilter() {
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration configuration = new CorsConfiguration();
+        
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","PATCH"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Origin", "Access-Control-Allow-Origin", "Content-Type", "Accept", "Jwt-Token", "Origin Accept", "X-Requested-With"));
+        configuration.setExposedHeaders(Arrays.asList("Authorization", "Origin", "Access-Control-Allow-Origin", "Content-Type", "Accept", "Jwt-Token", "Origin Accept", "Filename", "Access-Control-Allow-Credentials"));
+
+        source.registerCorsConfiguration("/**",configuration);
+        return new CorsFilter(source);
 	}
 
 }
